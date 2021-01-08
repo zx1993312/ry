4a107972e5b63a21a11a73f53338c469807d671c
zx1993312 <995760188@qq.com>
2021-01-08 11:45:38 +0800

    添加mapper注释

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/HyDataIsExistMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/HyDataIsExistMapper.java
@@ -5,9 +5,21 @@ import org.apache.ibatis.annotations.Select;
 
 import com.ruoyi.system.domain.HyDataIsExist;
 
+/**
+ * 校验数据是否存在Mapper
+ * 
+ * @author Administrator
+ *
+ */
 @Mapper
 public interface HyDataIsExistMapper {
 
+	/**
+	 * 查询指定表中数据是否存在
+	 * 
+	 * @param hyDataIsExist
+	 * @return
+	 */
 	@Select("select * from ${tableName} where ${fieldName}=#{param}")
 	public Object selectHyDataIsExist(HyDataIsExist hyDataIsExist);
 
