ece5c9a036cdb10979dd2fd32754c1cd2d77028e
liude <1109039844@qq.com>
2021-01-11 15:34:06 +0800

    添加

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/enums/ParkingEnum.java
@@ -0,0 +1,27 @@
+package com.ruoyi.system.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ParkingEnum {
+
+	STATE(0, "已使用"), DISSTATE(1, "未使用");
+
+	private int type;
+	private String value;
+
+	private ParkingEnum(int type, String value) {
+		this.type = type;
+		this.value = value;
+	}
+
+	public static String getValue(int type) {
+		for (ParkingEnum pe : ParkingEnum.values()) {
+			if (pe.type == type) {
+				return pe.value;
+			}
+		}
+			return null;
+		
+	}
+}
