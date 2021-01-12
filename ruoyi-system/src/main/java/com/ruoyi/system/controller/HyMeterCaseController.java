5601175eaaa1dac3c0f14a0a7070ce623fcc98d2
liude <1109039844@qq.com>
2021-01-12 16:43:06 +0800

    添加

--- ruoyi-system/src/main/java/com/ruoyi/system/controller/HyMeterCaseController.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/controller/HyMeterCaseController.java
@@ -1,6 +1,7 @@
 package com.ruoyi.system.controller;
 
 import java.util.List;
+
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.system.domain.HyMeter;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.system.domain.HyMeterCase;
 import com.ruoyi.system.domain.MeterAndCase;
 import com.ruoyi.system.service.IHyMeterCaseService;
@@ -21,11 +26,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-
-import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.common.core.page.TableDataInfo;
 
 /**
  * 箱
@@ -93,7 +93,7 @@ public class HyMeterCaseController extends BaseController
 
      */
     @GetMapping("/add")
-    public String add()
+    public String add(MeterAndCase meterAndCase)
     {
         return prefix + "/add";
     }
