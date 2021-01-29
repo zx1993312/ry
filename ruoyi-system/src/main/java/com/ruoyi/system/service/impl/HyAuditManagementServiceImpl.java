299090c5107da5f2482600fd63f0becce8e508ec
zx1993312 <995760188@qq.com>
2021-01-29 19:05:19 +0800

    1

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/HyAuditManagementServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/HyAuditManagementServiceImpl.java
@@ -3,106 +3,99 @@ package com.ruoyi.system.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ruoyi.system.mapper.HyAuditManagementMapper;
 import com.ruoyi.system.domain.HyAuditManagement;
 import com.ruoyi.system.service.IHyAuditManagementService;
 import com.ruoyi.common.core.text.Convert;
 
 /**
- * 审核管理
-Service业务层处理
+ * 审核管理 Service业务层处理
  * 
  * @author Administrator
  * @date 2021-01-23
  */
 @Service
-public class HyAuditManagementServiceImpl implements IHyAuditManagementService 
-{
-    @Autowired
-    private HyAuditManagementMapper hyAuditManagementMapper;
+public class HyAuditManagementServiceImpl implements IHyAuditManagementService {
+	@Autowired
+	private HyAuditManagementMapper hyAuditManagementMapper;
 
-    /**
-     * 查询审核管理
+	/**
+	 * 查询审核管理
+	 * 
+	 * 
+	 * @param id 审核管理 ID
+	 * @return 审核管理
+	 * 
+	 */
+	@Override
+	public HyAuditManagement selectHyAuditManagementById(Long id) {
+		return hyAuditManagementMapper.selectHyAuditManagementById(id);
+	}
 
-     * 
-     * @param id 审核管理
-ID
-     * @return 审核管理
+	/**
+	 * 查询审核管理 列表
+	 * 
+	 * @param hyAuditManagement 审核管理
+	 * 
+	 * @return 审核管理
+	 * 
+	 */
+	@Override
+	public List<HyAuditManagement> selectHyAuditManagementList(HyAuditManagement hyAuditManagement) {
+		return hyAuditManagementMapper.selectHyAuditManagementList(hyAuditManagement);
+	}
 
-     */
-    @Override
-    public HyAuditManagement selectHyAuditManagementById(Long id)
-    {
-        return hyAuditManagementMapper.selectHyAuditManagementById(id);
-    }
+	/**
+	 * 新增审核管理
+	 * 
+	 * 
+	 * @param hyAuditManagement 审核管理
+	 * 
+	 * @return 结果
+	 */
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public int insertHyAuditManagement(HyAuditManagement hyAuditManagement) {
+		return hyAuditManagementMapper.insertHyAuditManagement(hyAuditManagement);
+	}
 
-    /**
-     * 查询审核管理
-列表
-     * 
-     * @param hyAuditManagement 审核管理
+	/**
+	 * 修改审核管理
+	 * 
+	 * 
+	 * @param hyAuditManagement 审核管理
+	 * 
+	 * @return 结果
+	 */
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public int updateHyAuditManagement(HyAuditManagement hyAuditManagement) {
+		return hyAuditManagementMapper.updateHyAuditManagement(hyAuditManagement);
+	}
 
-     * @return 审核管理
+	/**
+	 * 删除审核管理 对象
+	 * 
+	 * @param ids 需要删除的数据ID
+	 * @return 结果
+	 */
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public int deleteHyAuditManagementByIds(String ids) {
+		return hyAuditManagementMapper.deleteHyAuditManagementByIds(Convert.toStrArray(ids));
+	}
 
-     */
-    @Override
-    public List<HyAuditManagement> selectHyAuditManagementList(HyAuditManagement hyAuditManagement)
-    {
-        return hyAuditManagementMapper.selectHyAuditManagementList(hyAuditManagement);
-    }
-
-    /**
-     * 新增审核管理
-
-     * 
-     * @param hyAuditManagement 审核管理
-
-     * @return 结果
-     */
-    @Override
-    public int insertHyAuditManagement(HyAuditManagement hyAuditManagement)
-    {
-        return hyAuditManagementMapper.insertHyAuditManagement(hyAuditManagement);
-    }
-
-    /**
-     * 修改审核管理
-
-     * 
-     * @param hyAuditManagement 审核管理
-
-     * @return 结果
-     */
-    @Override
-    public int updateHyAuditManagement(HyAuditManagement hyAuditManagement)
-    {
-        return hyAuditManagementMapper.updateHyAuditManagement(hyAuditManagement);
-    }
-
-    /**
-     * 删除审核管理
-对象
-     * 
-     * @param ids 需要删除的数据ID
-     * @return 结果
-     */
-    @Override
-    public int deleteHyAuditManagementByIds(String ids)
-    {
-        return hyAuditManagementMapper.deleteHyAuditManagementByIds(Convert.toStrArray(ids));
-    }
-
-    /**
-     * 删除审核管理
-信息
-     * 
-     * @param id 审核管理
-ID
-     * @return 结果
-     */
-    @Override
-    public int deleteHyAuditManagementById(Long id)
-    {
-        return hyAuditManagementMapper.deleteHyAuditManagementById(id);
-    }
+	/**
+	 * 删除审核管理 信息
+	 * 
+	 * @param id 审核管理 ID
+	 * @return 结果
+	 */
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public int deleteHyAuditManagementById(Long id) {
+		return hyAuditManagementMapper.deleteHyAuditManagementById(id);
+	}
 }
