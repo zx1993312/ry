ef8bd22f5f053aa5d27199748766d83335ca3bc0
yanqiao <mr.robort95@gmail.com>
2021-03-10 11:25:49 +0800

    11

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/controller/HyMapController.java
@@ -0,0 +1,31 @@
+package com.ruoyi.system.controller;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.system.service.IHyBusinessService;
+
+import io.swagger.annotations.Api;
+
+/**
+ * 商家Controller
+ * 
+ * @author Administrator
+ * @date 2021-03-05
+ */
+@Controller
+@RequestMapping("/system/map")
+@Api(tags = "商家Controller")
+public class HyMapController extends BaseController {
+	private String prefix = "system/baiduMap";
+
+	@RequiresPermissions("system:business:view")
+	@GetMapping()
+	public String business() {
+		return prefix + "/baidu";
+	}
+}
