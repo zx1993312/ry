9bbb9996f3b33964096adeeafd6ae8edb7b7cc0f
664772753@qq.com <Administrator@hy001>
2021-06-07 10:47:47 +0800

    修改

--- ruoyi-admin/src/main/resources/static/ruoyi/js/equipmentKanban.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/js/equipmentKanban.js
@@ -1,16 +1,20 @@
-var chartDom = document.getElementById('ceshi1');
-var myChart = echarts.init(chartDom);
-var option = {
-		xAxis : {
-			type : 'category',
-			data : [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ]
-		},
-		yAxis : {
-			type : 'value'
-		},
-		series : [ {
-			data : [ 120, 200, 150, 80, 70, 110, 130 ],
-			type : 'bar'
-		} ]
-	};
-myChart.setOption(option);
+$(function () {
+	ceshis1();
+    function ceshis1() {
+    	var myChart = echarts.init(document.getElementById('ceshi1'));
+    	var option = {
+    	    xAxis: {
+    	        type: 'category',
+    	        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    	    },
+    	    yAxis: {
+    	        type: 'value'
+    	    },
+    	    series: [{
+    	        data: [120, 200, 150, 80, 70, 110, 130],
+    	        type: 'bar'
+    	    }]
+    	};
+    	myChart.setOption(option);
+    }
+});
