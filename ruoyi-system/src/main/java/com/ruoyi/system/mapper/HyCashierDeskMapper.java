824d64cd3e22c84062bea5942143f19147b7d7e6
zx1993312 <995760188@qq.com>
2021-06-21 17:17:37 +0800

    修改

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/HyCashierDeskMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/HyCashierDeskMapper.java
@@ -2,6 +2,8 @@ package com.ruoyi.system.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.ruoyi.system.domain.HyCost;
 
 /**
@@ -10,39 +12,46 @@ import com.ruoyi.system.domain.HyCost;
  * @author Administrator
  * @date 2021-01-06
  */
-public interface HyCashierDeskMapper 
-{
-    /**
-     * 查询收银台
-     * 
-     * @param id 收银台ID
-     * @return 收银台
-     */
-    public HyCost selectHyCashierDeskById(Long id);
+public interface HyCashierDeskMapper {
+	/**
+	 * 查询收银台
+	 * 
+	 * @param id 收银台ID
+	 * @return 收银台
+	 */
+	public HyCost selectHyCashierDeskById(Long id);
 
-    /**
-     * 查询收银台列表
-     * 
-     * @param hyCost 收银台
-     * @return 收银台集合
-     */
-    public List<HyCost> selectHyCashierDeskList(HyCost hyCost);
-    
-    /**
-     * 根据已支付未支付查询收银台列表
-     * 
-     * @param hyCost 收银台
-     * @return 收银台集合
-     */
-    public List<HyCost> selectHyCashierDeskListByIsCollection(HyCost hyCost);
-    
-    /**
-     * 修改收银台
-     * 
-     * @param hyCost 收银台
-     * @return 结果
-     */
-    public int updateHyCashierDesk(HyCost hyCost);
+	/**
+	 * 查询收银台列表
+	 * 
+	 * @param hyCost 收银台
+	 * @return 收银台集合
+	 */
+	public List<HyCost> selectHyCashierDeskList(HyCost hyCost);
 
+	/**
+	 * 根据已支付未支付查询收银台列表
+	 * 
+	 * @param hyCost 收银台
+	 * @return 收银台集合
+	 */
+	public List<HyCost> selectHyCashierDeskListByIsCollection(HyCost hyCost);
+
+	/**
+	 * 根据已支付未支付查询收银台列表
+	 * 
+	 * @param hyCost 收银台
+	 * @return 收银台集合
+	 */
+	public List<HyCost> selectHyReportDataCollection(@Param("houseNumber") String houseNumber,
+			@Param("ownerId") Long ownerId);
+
+	/**
+	 * 修改收银台
+	 * 
+	 * @param hyCost 收银台
+	 * @return 结果
+	 */
+	public int updateHyCashierDesk(HyCost hyCost);
 
 }
