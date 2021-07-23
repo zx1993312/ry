6fa768e9d35822ac40d37d26d851146b6154dd34
zx1993312 <995760188@qq.com>
2021-07-23 16:47:53 +0800

    添加定时调度

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/HyNodeTaskServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/HyNodeTaskServiceImpl.java
@@ -1,6 +1,5 @@
 package com.ruoyi.system.service.impl;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import org.quartz.Scheduler;
@@ -9,14 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ruoyi.system.mapper.HyCheckNodeMapper;
-import com.ruoyi.system.mapper.HyNodeTaskMapper;
-import com.ruoyi.system.mapper.TaskAndNodeMapper;
-import com.ruoyi.system.constants.Constants;
-import com.ruoyi.system.domain.HyCheckNode;
-import com.ruoyi.system.domain.HyNodeTask;
-import com.ruoyi.system.domain.TaskAndNode;
-import com.ruoyi.system.service.IHyNodeTaskService;
 import com.ruoyi.common.constant.ScheduleConstants;
 import com.ruoyi.common.core.text.Convert;
 import com.ruoyi.common.exception.job.TaskException;
@@ -24,6 +15,15 @@ import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.quartz.domain.SysJob;
 import com.ruoyi.quartz.mapper.SysJobMapper;
 import com.ruoyi.quartz.util.ScheduleUtils;
+import com.ruoyi.system.constants.Constants;
+import com.ruoyi.system.domain.HyCheckNode;
+import com.ruoyi.system.domain.HyNodeTask;
+import com.ruoyi.system.domain.TaskAndNode;
+import com.ruoyi.system.mapper.HyCheckNodeMapper;
+import com.ruoyi.system.mapper.HyNodeTaskMapper;
+import com.ruoyi.system.mapper.TaskAndNodeMapper;
+import com.ruoyi.system.service.IHyNodeTaskService;
+import com.ruoyi.system.utils.HyListUtil;
 
 /**
  * 节点巡检任务Service业务层处理
@@ -51,7 +51,8 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 查询节点巡检任务
 	 * 
-	 * @param id 节点巡检任务ID
+	 * @param id
+	 *            节点巡检任务ID
 	 * @return 节点巡检任务
 	 */
 	@Override
@@ -62,7 +63,8 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 查询节点巡检任务列表
 	 * 
-	 * @param hyNodeTask 节点巡检任务
+	 * @param hyNodeTask
+	 *            节点巡检任务
 	 * @return 节点巡检任务
 	 */
 	@Override
@@ -73,7 +75,8 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 查询节点巡检任务未完成列表
 	 * 
-	 * @param hyNodeTask 节点巡检任务
+	 * @param hyNodeTask
+	 *            节点巡检任务
 	 * @return 节点巡检任务
 	 */
 	@Override
@@ -84,7 +87,8 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 查询节点巡检任务进行中列表
 	 * 
-	 * @param hyNodeTask 节点巡检任务
+	 * @param hyNodeTask
+	 *            节点巡检任务
 	 * @return 节点巡检任务
 	 */
 	@Override
@@ -95,7 +99,8 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 查询节点巡检任务已完成列表
 	 * 
-	 * @param hyNodeTask 节点巡检任务
+	 * @param hyNodeTask
+	 *            节点巡检任务
 	 * @return 节点巡检任务
 	 */
 	@Override
@@ -106,7 +111,8 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 新增节点巡检任务
 	 * 
-	 * @param hyNodeTask 节点巡检任务
+	 * @param hyNodeTask
+	 *            节点巡检任务
 	 * @return 结果
 	 * @throws TaskException
 	 * @throws SchedulerException
@@ -135,7 +141,7 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 					SysJob job = new SysJob();
 					job.setJobName(hyNodeTask.getTaskName());
 					job.setJobGroup("DEFAULT");
-					job.setInvokeTarget("hangYuTaskUtil.updateTaskAndNode()");
+					job.setInvokeTarget("hangYuTaskUtil.updateTaskAndNode(" + hyNodeTask.getId() + ")");
 					job.setMisfirePolicy("1");
 					job.setConcurrent("1");
 					job.setStatus("0");
@@ -176,7 +182,8 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 修改节点巡检任务
 	 * 
-	 * @param hyNodeTask 节点巡检任务
+	 * @param hyNodeTask
+	 *            节点巡检任务
 	 * @return 结果
 	 */
 	@Transactional(rollbackFor = Exception.class)
@@ -192,7 +199,7 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 				ids = ids + nodeId + ",";
 			}
 			String[] id = ids.split(",");
-			String[] iii = minus(nodeIds, id);
+			String[] iii = HyListUtil.minus(nodeIds, id);
 			for (String i : iii) {
 				TaskAndNode taskAndNode = new TaskAndNode();
 				taskAndNode.setTaskId(taskId);
@@ -215,49 +222,40 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 
 			}
 		}
+		
+		SysJob job = new SysJob();
+		switch (hyNodeTask.getTaskTime()) {
+		case "1":
+			job.setCronExpression(Constants.CRON_DAY);
+			break;
+		case "2":
+			job.setCronExpression(Constants.CRON_WEEK);
+			break;
+		case "3":
+			job.setCronExpression(Constants.CRON_MONTH);
+			break;
+		}
+		
+		job.setInvokeTarget("hangYuTaskUtil.updateTaskAndNode(" + hyNodeTask.getId() + ")");
+		jobMapper.updateJobByInvokeTarget(job);
+		
 		return hyNodeTaskMapper.updateHyNodeTask(hyNodeTask);
-	}
-
-	// 求两个数组的差集
-	public static String[] minus(String[] arr1, String[] arr2) {
-		LinkedList<String> list = new LinkedList<>();
-		LinkedList<String> history = new LinkedList<>();
-		String[] longerArr = arr1;
-		String[] shorterArr = arr2;
-		// 找出较长的数组来减较短的数组
-		if (arr1.length > arr2.length) {
-			longerArr = arr2;
-			shorterArr = arr1;
-		}
-		for (String str : longerArr) {
-			if (!list.contains(str)) {
-				list.add(str);
-			}
-		}
-		for (String str : shorterArr) {
-			if (list.contains(str)) {
-				history.add(str);
-				list.remove(str);
-			} else {
-				if (!history.contains(str)) {
-					list.add(str);
-				}
-			}
-		}
-
-		String[] result = {};
-		return list.toArray(result);
 	}
 
 	/**
 	 * 删除节点巡检任务对象
 	 * 
-	 * @param ids 需要删除的数据ID
+	 * @param ids
+	 *            需要删除的数据ID
 	 * @return 结果
 	 */
 	@Transactional(rollbackFor = Exception.class)
 	@Override
 	public int deleteHyNodeTaskByIds(String ids) {
+		String[] id = Convert.toStrArray(ids);
+		for (String taskId : id) {
+			jobMapper.deleteJobByInvokeTarget("hangYuTaskUtil.updateTaskAndNode(" + taskId + ")");// 删除定时调度任务
+		}
 		taskAndNodeMapper.deleteTaskAndNodeByIds(Convert.toStrArray(ids));
 		return hyNodeTaskMapper.deleteHyNodeTaskByIds(Convert.toStrArray(ids));
 	}
@@ -265,12 +263,14 @@ public class HyNodeTaskServiceImpl implements IHyNodeTaskService {
 	/**
 	 * 删除节点巡检任务信息
 	 * 
-	 * @param id 节点巡检任务ID
+	 * @param id
+	 *            节点巡检任务ID
 	 * @return 结果
 	 */
 	@Transactional(rollbackFor = Exception.class)
 	@Override
 	public int deleteHyNodeTaskById(Long id) {
+		jobMapper.deleteJobByInvokeTarget("hangYuTaskUtil.updateTaskAndNode(" + id + ")");// 删除定时调度任务
 		taskAndNodeMapper.deleteTaskAndNodeById(id);
 		return hyNodeTaskMapper.deleteHyNodeTaskById(id);
 	}
--- ruoyi-system/src/main/java/com/ruoyi/system/utils/HangYuTaskUtil.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/utils/HangYuTaskUtil.java
@@ -1,12 +1,31 @@
 package com.ruoyi.system.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.ruoyi.system.domain.TaskAndNode;
+import com.ruoyi.system.mapper.TaskAndNodeMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Component("hangYuTaskUtil")
+@Slf4j
 public class HangYuTaskUtil {
 
-	public void updateTaskAndNode() {
-		System.out.println("11111111111111执行无参方法");
+	@Autowired
+	private TaskAndNodeMapper taskAndNodeMapper;
+
+	public void updateTaskAndNode(Integer taskId) {
+		try {
+			TaskAndNode taskAndNode = new TaskAndNode();
+			taskAndNode.setTaskId(Long.valueOf(taskId));
+			taskAndNode.setIsInspection("0");
+			int result = taskAndNodeMapper.updateTaskAndNode(taskAndNode);
+			log.info("修改数据成功！修改了" + result + "条数据");
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+
 	}
 
 }
